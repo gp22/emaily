4a73aeb908e81a0d4d0ecfe5d428774188c86d78
gp22 <gp22@tutanota.com>
2018-11-23 13:07:03 -0500

    initial commit

---
+++ config/dev.example.js
@@ -0,0 +1,9 @@
+// fill in the necessary keys and rename to dev.js for a development build
+module.exports = {
+  googleClientID: '',
+  googleClientSecret: '',
+  mongoURI: '',
+  cookieKey: '',
+  stripePublishableKey: '',
+  stripeSecretKey: '',
+};
